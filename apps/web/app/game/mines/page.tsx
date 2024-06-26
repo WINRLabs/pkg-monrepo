ecb53cd0ad97588eecef96a39695cd3018c4c876
WINRLabs <justb.serbia@gmail.com>
2024-06-26 11:50:37 +0300

    ✨ (page.tsx): create a new MinesPage component for the Mines game in the web app 📝 (index.ts): export the Mines game module to make it available for use in the games package

---
+++ apps/web/app/game/mines/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { MinesTemplate } from "@winrlabs/games";
+import React from "react";
+
+const MinesPage = () => {
+  return (
+    <div>
+      <MinesTemplate />
+    </div>
+  );
+};
+
+export default MinesPage;
